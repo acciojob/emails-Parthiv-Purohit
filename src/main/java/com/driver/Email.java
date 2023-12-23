4acb1c06339e8b5f4dd58938d9eb6186ac0ffa4a
Admin <purohitparthiv@gmail.com>
2023-12-24 01:31:04 +0530

    fix

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,70 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(this.password.equals(oldPassword))
+        {
+            boolean flag1 = false;
+            if(newPassword.length() >=8)
+            {
+                flag1 = true;
+            }
+            boolean flag2 = false;
+            for(int i=0;i<newPassword.length();i++)
+            {
+                if(newPassword.charAt(i)>='A' && newPassword.charAt(i)<='Z')
+                {
+                    flag2 = true;
+                    break;
+
+                }
+            }
+            boolean flag3 = false;
+            for(int i=0;i<newPassword.length();i++)
+            {
+                if(newPassword.charAt(i)>='a' && newPassword.charAt(i)<='z')
+                {
+                    flag3 = true;
+                    break;
+
+                }
+            }
+            boolean flag4 = false;
+            for(int i=0;i<newPassword.length();i++)
+            {
+                if(newPassword.charAt(i)>='0' && newPassword.charAt(i)<='9')
+                {
+                    flag4 = true;
+                    break;
+
+                }
+            }
+            boolean flag5 = false;
+            for(int i=0;i<newPassword.length();i++)
+            {
+                if (!Character.isDigit(newPassword.charAt(i))
+                    && !Character.isLetter(newPassword.charAt(i))
+                    && !Character.isWhitespace(newPassword.charAt(i)))
+                {
+                    flag5 = true;
+                    break;
+
+                }
+            }
+
+            if(flag1 && flag2 && flag3 && flag4 && flag5)
+            {
+                setPassword(newPassword);
+            }
+
+        }
+
+    }
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+    public void setEmailId(String email)
+    {
+        this.emailId = email;
     }
 }
